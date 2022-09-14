4a25d5b85b6798027c1b5efd9c26681352e1e161
hadi <hadi123>
2022-09-14 16:37:58 +0430

    fixing6

--- config/sso.php
+++ config/sso.php
@@ -1,6 +1,36 @@
 <?php
 return [
-	'sso_url'      => 'url'
-    'api_prefix'   => 'api',
-    'admin_prefix' => 'admin',
+
+    /*
+    |--------------------------------------------------------------------------
+    | SSO URL
+    |--------------------------------------------------------------------------
+    */
+    'url'      => '127.0.0.1:8000/service/v1/sso-auth',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routing
+    |--------------------------------------------------------------------------
+    */
+
+    // The prefix used in all base routes
+    'route_prefix'              =>  '',
+    // The prefix used in api endpoints
+    'api_prefix'                => 'api',
+    // The prefix used in admin route
+    'admin_prefix'              => 'admin',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware
+    |--------------------------------------------------------------------------
+    */
+
+    // route middleware
+    'route_middleware'          =>  ['web'],
+    // api middleware
+    'api_middleware'            =>  ['api'],
+    // admin middleware
+    'admin_middleware'          =>  ['web'],
 ];
